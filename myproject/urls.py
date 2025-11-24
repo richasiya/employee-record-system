b0ca64bb8fe30f9134cf6c31f787c8af5dbd9c97
richasiya <rk9640263@gmail.com>
2025-11-24 22:16:42 +0530

    Fixed header alignment and updated CSS

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,7 +23,7 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",home),
-    path("index/",home),
+    path("home/",home),
     path("about/",about),
     path("services/",services),
     path("emp/",include('emp.urls'))
